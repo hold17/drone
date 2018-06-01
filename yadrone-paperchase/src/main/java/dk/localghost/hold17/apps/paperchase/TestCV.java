7daa5d54f7b6c9868b73b5d26d9cf30633698bab
LAPTOP-2KGNLENP\Thoma <Thomaslienchristensen@gmail.com>
2018-06-01 15:16:26 +0200

    Successful color filtering images. Code modified to test videofeed filtering.

--- yadrone-paperchase/src/main/java/dk/localghost/hold17/apps/paperchase/TestCV.java
+++ yadrone-paperchase/src/main/java/dk/localghost/hold17/apps/paperchase/TestCV.java
@@ -1,8 +1,7 @@
 package dk.localghost.hold17.apps.paperchase;
 
-import org.opencv.core.Core;
+import org.opencv.core.*;
 import org.opencv.core.Mat;
-import org.opencv.core.MatOfByte;
 import org.opencv.imgcodecs.Imgcodecs;
 
 import javax.imageio.ImageIO;
@@ -11,9 +10,17 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.file.Paths;
+import org.bytedeco.javacv.*;
+import org.bytedeco.javacpp.*;
 
-import static org.opencv.imgproc.Imgproc.COLOR_BGR2GRAY;
-import static org.opencv.imgproc.Imgproc.cvtColor;
+import static java.lang.System.exit;
+import static org.bytedeco.javacpp.opencv_core.*;
+import static org.bytedeco.javacpp.opencv_highgui.*;
+import static org.bytedeco.javacpp.opencv_imgcodecs.cvLoadImage;
+import static org.bytedeco.javacpp.opencv_videoio.*;
+import static org.bytedeco.javacpp.opencv_imgproc.*;
+//import static org.opencv.imgproc.Imgproc.*;
+//import static org.opencv.imgproc.Imgproc.COLOR_BGR2GRAY;
 
 public class TestCV {
     public Mat testMat;
@@ -28,8 +35,9 @@ public class TestCV {
         try {
             testMat = openFile("a4-papir.jpg");
             System.out.println(testMat.toString());
-            convertGreyscale(testMat);
-            saveFile("imageOutput.jpg", grayImage);
+//            convertGreyscale(testMat);
+//            saveFile("imageOutput.jpg", grayImage);
+            detectWhite();
         } catch (Exception e) {
             System.err.println("Something went wrong: " + e.toString());
         }
@@ -47,6 +55,7 @@ public class TestCV {
         if (newImage.dataAddr() == 0) {
             throw new Exception("Couldn't open file " + filePath);
         }
+
         return newImage;
     }
 
@@ -70,8 +79,56 @@ public class TestCV {
         System.out.println("File saved to " + filePath);
     }
 
-    public void convertGreyscale(Mat testMat) {
-        grayImage = new Mat();
-        cvtColor(testMat, this.grayImage, COLOR_BGR2GRAY);
+//    public void convertGreyscale(Mat testMat) {
+//        grayImage = new Mat();
+//        cvtColor(testMat, this.grayImage, COLOR_BGR2GRAY);
+//    }
+
+    public void detectWhite() {
+
+        IplImage img1, imghsv, imgbin;
+        CvCapture cam;
+        if((cam = cvCaptureFromCAM(-1)) == null) {
+            System.out.println("Didn't find webcam. Exiting.");
+            exit(0);
+        }
+
+        imghsv = cvCreateImage(cvSize(1280,720), 8, 3);
+        imgbin = cvCreateImage(cvSize(1280,720), 8, 1);
+        cvQueryFrame(cam);
+
+        for(;;) {
+            img1 = cvQueryFrame(cam);
+            imghsv = cvCreateImage(cvGetSize(img1), 8, 3);
+            imgbin = cvCreateImage(cvGetSize(img1), 8, 1);
+            if(img1 == null) break;
+
+//            cvShowImage("webcam", img1);
+        cvCvtColor(img1, imghsv, CV_BGR2HSV);
+            CvScalar minc = cvScalar(210, 210, 210, 0);
+            CvScalar maxc = cvScalar(255, 255, 255, 0);
+            cvInRangeS(img1, minc, maxc, imgbin);
+
+            cvShowImage("default", img1);
+            cvShowImage("Binary", imgbin);
+            char c = (char) cvWaitKey(15);
+            if(c == 'q') break;
+        }
+
+        cvReleaseImage(imghsv);
+        cvReleaseImage(imgbin);
+        cvReleaseCapture(cam);
     }
+
+//    public void cameratest() {
+//        VideoCapture cap = new VideoCapture(CV_CAP_ANY);
+//
+//        if(!cap.isOpened()) {
+//            System.out.println("Camera not found. Exiting.");
+//        }
+//
+//        Mat frame = new Mat();
+//        cap.read(frame);
+//        cvShowImage()
+//    }
 }
