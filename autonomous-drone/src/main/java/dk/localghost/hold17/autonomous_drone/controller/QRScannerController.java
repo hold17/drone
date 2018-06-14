7eeb504f8810f58baf5f0060fd3fb2afe6ad5c05
awo-dev <anders@wiberg.tech>
2018-06-15 00:06:13 +0200

    Calculate QR distance by measuring top left and top right points from the QR code.

--- autonomous-drone/src/main/java/dk/localghost/hold17/autonomous_drone/controller/QRScannerController.java
+++ autonomous-drone/src/main/java/dk/localghost/hold17/autonomous_drone/controller/QRScannerController.java
@@ -9,23 +9,37 @@ public class QRScannerController implements TagListener, QrTracker {
     private Direction qrDirection = Direction.UNKNOWN;
     private Direction lastKnownQrDirection = Direction.UNKNOWN;
 
+    // Distance between the to top points of the qr code
+    private double horizontalTopDistance = 0;
+
     @Override
     public void onTag(Result result, float orientation) {
         if (result == null) {
             return;
         }
 
-        final double X = result.getResultPoints()[0].getX();
-        final double Y = result.getResultPoints()[0].getY();
+        double topLeftX = result.getResultPoints()[1].getX();
+        double topLeftY = result.getResultPoints()[1].getY();
 
-        qrDirection = Direction.findXDirection(X);
+        double topRightX = result.getResultPoints()[2].getX();
+        double topRightY = result.getResultPoints()[2].getY();
+
+        double horizontalTopDistance = topRightX - topLeftX;
+        double verticalTopDistance   = topRightY - topRightX;
+
+        this.horizontalTopDistance = horizontalTopDistance;
+
+        qrDirection = Direction.findXDirection(topLeftX);
 
         if (qrDirection != Direction.UNKNOWN)
             lastKnownQrDirection = qrDirection;
 
         lastScan = result.getText();
 
-        System.out.println("QR Scanned, Result: " + lastScan + " at " + X + ", " + Y + "\tDirection: " + qrDirection);
+        System.out.println("QR Scanned, Result: " + lastScan + " at " + topLeftX + ", " + topLeftY + "\tDirection: " + qrDirection);
+        System.out.println("Top Horizontal Distance: " + horizontalTopDistance);
+        System.out.println("Top Vertical Distance  : " + verticalTopDistance);
+        System.out.println("--------------------------------");
     }
 
     public String getLastScan() {
@@ -49,11 +63,12 @@ public class QRScannerController implements TagListener, QrTracker {
     @Override
     public void resetFlightDirection() {
         qrDirection = Direction.UNKNOWN;
+        horizontalTopDistance = 0;
         resetLastScan();
     }
 
     @Override
     public boolean readyForFlyingThroughRing() {
-        return false;
+        return horizontalTopDistance > 170;
     }
 }
